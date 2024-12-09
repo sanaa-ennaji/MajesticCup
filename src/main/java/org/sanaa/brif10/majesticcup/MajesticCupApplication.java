a96b973e527ce1b44d7a3e693e236a618f7d1c30
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-12-09 17:22:50 +0100

    init

---
+++ src/main/java/org/sanaa/brif10/majesticcup/MajesticCupApplication.java
@@ -0,0 +1,13 @@
+package org.sanaa.brif10.majesticcup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajesticCupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MajesticCupApplication.class, args);
+    }
+
+}
